311e6106cf2d2fcb1a0860e2c6e1c062d0cca78f
Olaoluwa Daniel Jimoh <olaoluwajimohdaniel@gmail.com>
2023-02-19 12:08:46 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing number 0-9 with the putchar function
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a < 10; a++)
+	{
+		putchar((a % 10) + '0');
+	}
+	putchar ('\n');
+	return (0);
+}
