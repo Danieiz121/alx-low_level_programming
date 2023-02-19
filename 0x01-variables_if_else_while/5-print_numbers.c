27e9e34d70883a8d6ea38fb1b07440b8a71ff135
Olaoluwa Daniel Jimoh <olaoluwajimohdaniel@gmail.com>
2023-02-19 12:29:26 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-#include <stdlio.h>
-#include <time.h>
 /**
  * main - a program printing single digit numbers 0-9
  * Return: Always 0
  */
 int main(void)
 {
-	int c;
-	for (c = 0; c < 10; c++);
-	{
-		printf("%d", c);
-	}
-	printf('\n');
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
 	return (0);
 }
 
