6389e9610863de92b9b1e854ad85d9bb3b9cf50a
Olaoluwa Daniel Jimoh <olaoluwajimohdaniel@gmail.com>
2023-02-23 06:07:46 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_number(void);
+void more_number(void);
+void print_line(void);
+void print_diagona(int n);
+void print_square(int size);
+void print_trianglet(int size);
+void print_number(int n);
+
