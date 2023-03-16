5ce5c9168005237667b6b466548a45b36250e0b8
Olaoluwa Daniel Jimoh <olaoluwajimohdaniel@gmail.com>
2023-03-16 09:02:14 -0700

    master on it

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,33 +1,26 @@
-#include <stdlib.h>
-/** 
-  * _strncmp - compares two strings within b bytes. 
-  * @s1: string to check 
-  * @s2: string to check from 
-  * @b: number of bytes to check 
-  * Return: 0 if strings are the same 
-  * any other number if they are different. 
-  */ 
- int _strncmp(char *s1, char *s2, unsigned int b) 
- { 
-         unsigned int i; 
-         for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++) 
-         { 
-                 s1++; 
-                 s2++; 
-         } 
-         return (i - b); 
- } 
- /** 
-  * _strstr - locates a substring 
-  * @haystack: string to look though 
-  * @needle: substring to find 
-  * Return: pointer to string with result of search 
-  */ 
- char *_strstr(char *haystack, char *needle) 
- { 
-         unsigned int len; 
-         /Get length of needle for strncmp/ 
-         len = 0; 
-         while (needle[len] != '\0') 
-                 len++; 
-         /compare substring/
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strstr - This function takes two strings s1 and s2 as an argument
+ * @s1: This is the main string to be examined.
+ * @s2: This is the sub-string to be searched in s1 string.
+ * Return: value 0
+ */
+
+int main(void)
+{
+	char s1[] = "GeeksforGeeks";
+
+	char s2[] = "for";
+
+	char *p;
+
+	p = strstr(s1, s2);
+
+	if (p)
+	{
+		printf("String found\n");
+		printf("First occurrence of string '%s' in '%s' is '%s'", s2, s1, p);
+	}
+	return 0;
+}
