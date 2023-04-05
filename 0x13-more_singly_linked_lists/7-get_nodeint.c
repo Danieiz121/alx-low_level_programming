2c520be7a11a0741c2678fdd1a0576726b4c2da5
Olaoluwa Daniel Jimoh <olaoluwajimohdaniel@gmail.com>
2023-04-05 07:10:55 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- *  * get_nodeint_at_index - get node at index
- *   * @head: head node
- *    * @index: index node
- *     * Return: address of index node
- *      */
+ * get_nodeint_at_index - get node at index
+ * @head: head node
+ * @index: index node
+ * Return: address of index node
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
