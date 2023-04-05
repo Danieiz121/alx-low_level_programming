272aa960c495f0d5fec39228188526145c8f8adb
Olaoluwa Daniel Jimoh <olaoluwajimohdaniel@gmail.com>
2023-04-05 07:07:51 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- *  * free_listint - frees a list
- *   * @head: head node
- *    * Return: void
- *     */
+ * free_listint - frees a list
+ * @head: head node
+ * Return: void
+ */
 void free_listint(listint_t *head)
 {
 	while (head)
