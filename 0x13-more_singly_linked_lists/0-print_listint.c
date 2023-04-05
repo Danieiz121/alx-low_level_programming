0207aa126fcc116355c7c621721e033300b5eeec
Olaoluwa Daniel Jimoh <olaoluwajimohdaniel@gmail.com>
2023-04-05 06:01:57 -0700

    0-main.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,11 @@ size_t print_listint(const listint_t *h)
 {
 	int nodes = 0;
 
-	while (d)
+	while (h)
 	{
 		nodes++;
 		printf("%d", h->n);
-		d = d->next;
+		h = h->next;
 		printf("\n");
 	}
 	return (nodes);
